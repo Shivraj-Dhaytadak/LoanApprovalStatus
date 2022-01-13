109157cbadccaea88daa02a8d95d0ea9c79582ba
Shivraj-Dhaytadak <67308124+Shivraj-Dhaytadak@users.noreply.github.com>
2022-01-13 14:17:22 +0530

    Minor Changes and Pathing

--- Test/LoanStatus/temp.py
+++ Test/LoanStatus/temp.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, url_for, redirect, session
 import pymongo
 import bcrypt
@@ -7,10 +8,13 @@ import numpy as np
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 app.secret_key = "6042d70a-20c4-4049-a6dc-e9a244903532"
-client = pymongo.MongoClient("mongodb+srv://flaskloan:flaskloan@cluster0.ogg9d.mongodb.net/login?ssl=true&ssl_cert_reqs=CERT_NONE")
+client = pymongo.MongoClient(
+    "mongodb+srv://flaskloan:flaskloan@cluster0.ogg9d.mongodb.net/login?ssl=true&ssl_cert_reqs=CERT_NONE")
 db = client.get_database('login')
 records = db.user
 Adminrecord = db.admin
+LoanApplication = db.LoanApplication
+
 
 @app.route("/", methods=['post', 'get'])
 def userLog():
@@ -20,7 +24,7 @@ def userLog():
     if request.method == "POST":
         user = request.form.get("user[username]")
         email = request.form.get("user[email]")
-        
+
         password1 = request.form.get("user[password]")
         password2 = request.form.get("user[cpassword]")
         user_found = records.find_one({"name": user})
@@ -38,10 +42,10 @@ def userLog():
             hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
             user_input = {'name': user, 'email': email, 'password': hashed}
             records.insert_one(user_input)
-            
+
             user_data = records.find_one({"email": email})
             new_email = user_data['email']
-   
+
             return render_template('userDashboard.html', email=new_email)
     return render_template('userLogin.html')
 
@@ -54,13 +58,15 @@ def userDashboard():
     else:
         return redirect(url_for("userLogin"))
 
+
 @app.route('/loanapply')
 def loanapply():
     if "email" in session:
         email = session["email"]
         return render_template('LoanApply.html')
 
-@app.route('/applyforloan',methods=['POST','GET'])
+
+@app.route('/applyforloan', methods=['POST', 'GET'])
 def predict():
 
     int_features = [int(x) for x in request.form.values()]
@@ -86,14 +92,14 @@ def userlogin():
         email = request.form.get("user[email]")
         password = request.form.get("user[password]")
 
-        admin_found = Adminrecord.find_one({"email":email})
+        admin_found = Adminrecord.find_one({"email": email})
         email_found = records.find_one({"email": email})
         if admin_found:
             return render_template('adminLogin.html')
         if email_found:
             email_val = email_found['email']
             passwordcheck = email_found['password']
-            
+
             if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
                 session["email"] = email_val
                 return redirect(url_for('userDashboard'))
@@ -107,13 +113,40 @@ def userlogin():
             return render_template('userLogin.html', message=message)
     return render_template('userLogin.html', message=message)
 
+
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
     if "email" in session:
         session.pop("email", None)
         return render_template("userLogin.html")
     else:
-        return render_template('index.html')
-      
+        return render_template('userLogin.html')
+
+
+@app.route('/loadAdmin', methods=['POST'])
+def loadAdmin():
+    return render_template('adminLogin.html')
+
+
+@app.route('/adminlogin', methods=['POST'])
+def adminlogin():
+    if request.method == "POST":
+        email = request.form.get("admin[username]")
+        password = request.form.get("admin[password]")
+        admin_found = Adminrecord.find_one({"email": email})
+        if admin_found:
+            email_val = admin_found['email']
+            passwordcheck = admin_found['password']
+
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return render_template('adminDashboard.html')
+            else:
+                if "email" in session:
+                    return render_template('adminDashboard.html')
+                message = 'Wrong password'
+                return render_template('adminlogin.html', message=message)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
