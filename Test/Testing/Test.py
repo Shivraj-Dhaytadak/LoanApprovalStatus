f7ace35b70b2547365ed94cfadb9d9b8c3e86348
Shivraj-Dhaytadak <67308124+Shivraj-Dhaytadak@users.noreply.github.com>
2022-02-09 11:43:56 +0530

    Login Testing Done For User And admin

--- Test/Testing/Test.py
+++ Test/Testing/Test.py
@@ -1,9 +1,111 @@
-from lib2to3.pgen2 import driver
+# import all required frameworks
 from time import time
+import unittest
 from selenium import webdriver
-import time
-driver = webdriver.Chrome(
-    r"D:\BE\BE Project\SEM-7-Progress\LoanApprovalStatus\Test\Testing\chromedriver.exe")
-driver.get("http://127.0.0.1:5000/")
-time.sleep(3)
-driver.close()
+from selenium.webdriver.common.keys import Keys
+
+# inherit TestCase Class and create a new test class
+
+
+class RunningTest(unittest.TestCase):
+
+    # initialization of webdriver
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            r"D:\BE\BE Project\SEM-7-Progress\LoanApprovalStatus\Test\Testing\chromedriver.exe")
+
+    # Test case method. It should always start with test_
+    def test_url_test(self):
+
+        # get driver
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000/")
+
+        # assertion to confirm if title has python keyword in it
+        self.assertIn("Bank", driver.title)
+
+    # cleanup method called after every test performed
+    def tearDown(self):
+        self.driver.close()
+
+
+class UserLogin(unittest.TestCase):
+    # initialization of webdriver
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            r"D:\BE\BE Project\SEM-7-Progress\LoanApprovalStatus\Test\Testing\chromedriver.exe")
+
+    def test_user_login(self):
+        # get driver
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000/")
+        driver.find_element_by_name(
+            "user[email]").send_keys("new@gmail.com")
+        driver.find_element_by_name("user[password]").send_keys("1234")
+        driver.find_element_by_xpath(
+            "/html/body/div[2]/div[2]/div[1]/form/input[3]").click()
+        self.assertEquals("Bank User Dashboard", driver.title)
+
+    # cleanup method called after every test performed
+
+    def test_user_logout(self):
+        # get driver
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000/")
+        driver.find_element_by_name(
+            "user[email]").send_keys("new@gmail.com")
+        driver.find_element_by_name("user[password]").send_keys("1234")
+        driver.find_element_by_xpath(
+            "/html/body/div[2]/div[2]/div[1]/form/input[3]").click()
+        driver.find_element_by_xpath(
+            "/html/body/div[1]/nav/ul/li[4]/a").click()
+        self.assertEquals("Bank", driver.title)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+class AdminLogin(unittest.TestCase):
+    # initialization of webdriver
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            r"D:\BE\BE Project\SEM-7-Progress\LoanApprovalStatus\Test\Testing\chromedriver.exe")
+
+    def test_admin_login(self):
+        # get driver
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000/")
+        driver.find_element_by_xpath(
+            "/html/body/div[1]/div/form/input").click()
+        driver.find_element_by_name(
+            "admin[username]").send_keys("admin101d@gmail.com")
+        driver.find_element_by_name("admin[password]").send_keys("admin@101")
+        driver.find_element_by_xpath(
+            "/html/body/div[1]/form/div/input").click()
+        self.assertEquals("Bank Admin Dashboard", driver.title)
+
+    # cleanup method called after every test performed
+
+    def test_admin_logout(self):
+        # get driver
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000/")
+        driver.find_element_by_xpath(
+            "/html/body/div[1]/div/form/input").click()
+        driver.find_element_by_name(
+            "admin[username]").send_keys("admin101d@gmail.com")
+        driver.find_element_by_name("admin[password]").send_keys("admin@101")
+        driver.find_element_by_xpath(
+            "/html/body/div[1]/form/div/input").click()
+        driver.find_element_by_xpath(
+            "/html/body/div[1]/nav/ul/li[3]/a").click()
+        self.assertEquals("Bank", driver.title)
+    # cleanup method called after every test performed
+
+    def tearDown(self):
+        self.driver.close()
+
+
+# execute the script
+if __name__ == "__main__":
+    unittest.main()
